b730ad6b37440cd9767c8c9818ab4a806b84383a
Vivek-Bapodariya <vtbvtb412@gmail.com>
2024-01-28 21:34:44 +0530

    01vitereact project add

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./Chai.jsx"
+
+
+function App() {
+
+  const username = "Chai aur code"
+
+  return (
+    <>
+    <Chai />
+    <h1>Chai aur react {username}</h1>    
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,9 @@
+
+
+function Chai() {
+  return (
+    <div>Learning react JS</div>
+  )
+}
+
+export default Chai
